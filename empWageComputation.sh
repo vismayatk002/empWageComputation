07d2334c2f8494006d004adbd210171ba3e6b0af
vismayatk002 <vismayatk002@gmail.com>
2021-05-31 23:25:18 +0530

    Removed Space

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
- echo " Welcome to Employee Wage Computation Program "
-
+echo " Welcome to Employee Wage Computation Program "
 
 isPresent=1
 isAbsent=0
@@ -13,42 +12,30 @@ partTime=0
 workingDays=20
 totalHrs=0
 totalAttendance=0
-
-
-
 while [[ $totalAttendance -lt 20 && $totalHrs -lt 100 ]]
 do
 	attendance=$(($RANDOM%2))
+	case $attendance in
+		$isPresent ) #echo "Employee is Present"
 
-case $attendance in
-	$isPresent ) #echo "Employee is Present"
+					empType=$(($RANDOM%2))
+					((totalAttendance++))	 	           
+	 	            case $empType in
+	 	            	$fullTime ) #echo "Employee Type :Full Time"
 
-				empType=$(($RANDOM%2))
-				((totalAttendance++))
- 	           
- 	            case $empType in
- 	            	$fullTime ) #echo "Employee Type :Full Time"
-								
-								totalHrs=$(($totalHrs + $fullTimeHr))
-								fullTimeEmpWage=$(($wagePerHr * $fullTimeHr))
-	    						monthlyEmpWage=$(($monthlyEmpWage + $fullTimeEmpWage))
-	    						
+									totalHrs=$(($totalHrs + $fullTimeHr))
+									fullTimeEmpWage=$(($wagePerHr * $fullTimeHr))
+		    						monthlyEmpWage=$(($monthlyEmpWage + $fullTimeEmpWage))
+		    				;;	
+		    			$partTime ) #echo "Employee Type :Part Time"
 
-	    				;;	
-	    			$partTime ) #echo "Employee Type :Part Time"
-
-								totalHrs=$(($totalHrs + $partTimeHr))
- 								partTimeEmpWage=$(($wagePerHr * $partTimeHr))
-	    						monthlyEmpWage=$(($monthlyEmpWage + $partTimeEmpWage))
- 	            		;;
-
- 	            			
-
- 	            esac
-
-		;;
-
-esac
+									totalHrs=$(($totalHrs + $partTimeHr))
+	 								partTimeEmpWage=$(($wagePerHr * $partTimeHr))
+		    						monthlyEmpWage=$(($monthlyEmpWage + $partTimeEmpWage))
+		    				;;
+	 	            esac
+			;;
+	esac
 done
 echo "Monthly Wage =" $monthlyEmpWage
 
