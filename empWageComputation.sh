0769f47c7a4c84e97bf658e834b51575121032c6
vismayatk002 <vismayatk002@gmail.com>
2021-05-31 22:08:27 +0530

    Update with Monthly Wages

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -11,6 +11,7 @@ partTimeHr=8
 fullTimeHr=16
 fullTime=1
 partTime=0
+workingDays=20
 randomnumber=$(($RANDOM%2))
 
 case $randomnumber in
@@ -20,13 +21,21 @@ case $randomnumber in
  	           
  	            case $empType in
  	            	$fullTime ) echo "Employee Type :Full Time"
+
 								fullTimeEmpWage=$(($wagePerHr*$fullTimeHr))
 	    						echo "Employee Wage =" $fullTimeEmpWage
+
+								monthlyEmpWage=$((($wagePerHr*$fullTimeHr) * $workingDays))
+	    						echo "Monthly Wage =" $monthlyEmpWage
+
 	    					;;	
 	    			$partTime ) echo "Employee Type :Part Time"
 
  								partTimeEmpWage=$(($wagePerHr*$partTimeHr))
-	    						echo "Employee Wage =" $partTimeEmpWage	
+	    						echo "Employee Wage =" $partTimeEmpWage
+
+	    						monthlyEmpWage=$((($wagePerHr*$partTimeHr) * $workingDays))
+	    						echo "Monthly Wage =" $monthlyEmpWage
  	            			;;
  	            esac
 
