64b1b5280c5735cdbcce69c029144aca30383616
vismayatk002 <vismayatk002@gmail.com>
2021-05-31 19:58:28 +0530

    Added :Partime Employee Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,25 +3,36 @@
  echo " Welcome to Employee Wage Computation Program "
 
 
-# Check Employee is Present or Absent
+# Add part time Employee Wage
 ispresent=1
 wagePerHr=20
-fullDayHr=8
+partTimeHr=8
+fullTimeHr=16
+fullTime=1
 randomnumber=$(($RANDOM%2))
-
 echo $randomnumber
 
 if [ $randomnumber -eq $ispresent ]
 
 then
- 
-	echo "employee is present"
-	dailyEmpWage=$(($wagePerHr*$fullDayHr))
-	echo "Daily Wage =" $dailyEmpWage
+	echo "Employee is Present"
+ 	empType=$(($RANDOM%2))
+ 	echo $empType
+ 	if [ $empType -eq $fullTime ]
+ 	then
+ 		echo "Employee Type :Full Time"
 
+ 		fullTimeEmpWage=$(($wagePerHr*$fullTimeHr))
+	    echo "Employee Wage =" $fullTimeEmpWage
+ 	else
+ 		echo "Employee Type :Part Time"
+
+ 		partTimeEmpWage=$(($wagePerHr*$partTimeHr))
+	    echo "Employee Wage =" $partTimeEmpWage	
+	fi
 else
 
- 	echo "employee is absent"
+ 	echo "Employee is Absent"
 
 fi
 
