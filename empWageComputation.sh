78cdc5b1ea632ffc5a0ed75169e59f99718b0948
vismayatk002 <vismayatk002@gmail.com>
2021-05-31 16:56:56 +0530

    Added : Code for Checking Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-echo " Welcome to Employee Wage Computation Program "
+ echo " Welcome to Employee Wage Computation Program "
+
+
+# Check Employee is Present or Absent
+ispresent=1
+
+randomnumber=$(($RANDOM%2))
+
+echo $randomnumber
+
+if [ $randomnumber -eq $ispresent ]
+
+then
+ 
+	echo "employee is present"
+
+else
+
+ 	echo "employee is absent"
+
+fi
