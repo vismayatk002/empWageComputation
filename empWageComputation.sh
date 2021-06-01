dd1c6fe95c5ab6d165564e740c520d7b23e12134
vismayatk002 <vismayatk002@gmail.com>
2021-06-01 10:34:58 +0530

    Added Function to get Work hours

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,35 +5,50 @@ echo " Welcome to Employee Wage Computation Program "
 isPresent=1
 isAbsent=0
 wagePerHr=20
-partTimeHr=8
-fullTimeHr=16
-fullTime=1
-partTime=0
 workingDays=20
 totalHrs=0
+fullTime=1
+partTime=0
 totalAttendance=0
+function workHrs(){
+	empType=$1
+	if [ $empType -eq $fullTime ]
+	then
+		fullTimeHr=16
+		workHr=$(($fullTimeHr))
+	else
+		partTimeHr=8
+		workHr=$(($partTimeHr))
+	fi
+	echo $workHr
+}
+
 while [[ $totalAttendance -lt 20 && $totalHrs -lt 100 ]]
 do
 	attendance=$(($RANDOM%2))
 	case $attendance in
 		$isPresent )
 				#echo "Employee is Present"
-
 				empType=$(($RANDOM%2))
+				#echo "Type" $empType
 				((totalAttendance++))
 				case $empType in
 					$fullTime )
 							#echo "Employee Type :Full Time"
-
-							totalHrs=$(($totalHrs + $fullTimeHr))
-							fullTimeEmpWage=$(($wagePerHr * $fullTimeHr))
+							#workHrs $empType
+							hr=$(workHrs $empType)
+							#echo $hr
+							totalHrs=$(($totalHrs + $hr))
+							fullTimeEmpWage=$(($wagePerHr * $hr))
 							monthlyEmpWage=$(($monthlyEmpWage + $fullTimeEmpWage))
 			    		;;
 					$partTime )
 							#echo "Employee Type :Part Time"
-
-							totalHrs=$(($totalHrs + $partTimeHr))
-							partTimeEmpWage=$(($wagePerHr * $partTimeHr))
+							#workHrs $empType
+							hr=$(workHrs $empType)
+							#echo $hr
+							totalHrs=$(($totalHrs + $hr))
+							partTimeEmpWage=$(($wagePerHr * $hr))
 							monthlyEmpWage=$(($monthlyEmpWage + $partTimeEmpWage))
     					;;
 				esac
